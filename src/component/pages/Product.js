3b61e028aa8d823684f22d40017ca3add99f708d
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2018-05-26 14:16:27 +0430

    api req with axios

--- src/component/pages/Product.js
+++ src/component/pages/Product.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Product extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state={
+            articles: {}
+        }
+    };
+    componentDidMount(){
         const { params } = this.props.match;
+        // console.log(params);
+        axios.get(`http://roocket.org/api/products/${params.ProductID}`)
+            .then((response)=>{
+                // console.log(response);
+                this.setState({
+                    articles :response.data.data,
+                });
+            })
+            .catch((error)=>{
+                // console.log(error);
+            })
+    };
+    render() {
+        // console.log(params);
+        const { articles } = this.state;
+        console.log(articles);
         return (
         <div className="row">
             <div className="col s12">
                 <div className="card blue-grey darken-1">
                     <div className="card-content white-text">
-                        <span className="card-title">Product Detail {params.ProductID}</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title">Product Detail {articles.id}</span>
+                        <p>
+                            {/*I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.*/}
+                            {articles.body}
+                        </p>
                     </div>
                 </div>
             </div>
