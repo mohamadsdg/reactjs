0fcacc86ffc5bbee9a05c4865c414458ed09c744
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2018-05-26 11:29:58 +0430

    Router part II

--- src/AppSPA.js
+++ src/AppSPA.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Header from "./component/sections/Header";
-import {Route} from 'react-router-dom';
+import {Route ,Switch} from 'react-router-dom';
 
 // Components
 import Home from './component/pages/Home';
 import Contact from './component/pages/Contact';
 import About from './component/pages/About';
 import Product from "./component/pages/Product";
+import Not_found from "./component/pages/Not_found";
 
 class App extends Component {
     render() {
@@ -14,10 +15,13 @@ class App extends Component {
             <div>
                 <Header />
                 <div className='container'>
-                    <Route path='/' exact={true} component={Home}/>
-                    <Route path='/product/:ProductID' component={Product}/>
-                    <Route path='/contact_us' component={Contact}/>
-                    <Route path='/about_us' component={About}/>
+                    <Switch>
+                        <Route path='/' exact={true} component={Home}/>
+                        <Route path='/product/:ProductID' component={Product}/>
+                        <Route path='/contact_us' component={Contact}/>
+                        <Route path='/about_us' component={About}/>
+                        <Route component={Not_found}/>
+                    </Switch>
                 </div>
             </div>
         );
---
+++ src/component/pages/Not_found.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Not_found extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Not_found page</h1>
+            </div>
+        );
+    }
+}
+
+export default Not_found;
